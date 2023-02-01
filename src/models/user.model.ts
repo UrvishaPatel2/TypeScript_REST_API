b472b57d84528619fd3d69df18fc2678f87a0ae2
Urvisha Patel <urvisha.shivinfotech@gmail.com>
2023-02-01 19:34:13 +0530

    Add-changes-01-02-2023

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcrypt';
-import config  from "process";
+import bcrypt, { compare } from 'bcrypt';
+import config  from "config";
 
 export interface UserDocument extends mongoose.Document {
     email: string;
     name: string;
     password: string;
-    creatAt: Date;
+    createdAt: Date;
     updatedAt: Date;
+    comparePassword(candidatePassword: string): Promise<Boolean>
 }
 
 
@@ -19,16 +20,26 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 })
 
-userSchema.pre("save", async function (next: mongoose.HookNextFunction) {
+userSchema.pre("save", async function (next) {
     let user = this as UserDocument;
     
     if(!user.isModified("password")) {
         return next();
     }
 
-    const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"))
+    const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+    const hash = await bcrypt.hashSync(user.password, salt);
+
+    user.password = hash;
+    return next();
 })
 
-const UserModel = mongoose.model("User", userSchema, "User");
+userSchema.methods.comparePassword = async function(candidatePassword: string): Promise<boolean> {
+    const user = this as UserDocument;
+
+    return bcrypt.compare(candidatePassword, user.password).catch(e => false)
+}
+
+const UserModel = mongoose.model<UserDocument>("User", userSchema, "User");
 
 export default UserModel;
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,12 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
+import validResource from './middleware/validateResource';
+import { createUserSchema } from "./schema/user.schema";
 
 function routes(app: Express) {
     app.get('/healthCheck', (req:Request, res:Response) => res.sendStatus(200))
+
+    app.post("/api/users",validResource(createUserSchema), createUserHandler)
 }
 
 export default routes;
